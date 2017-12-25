d7841ff81dfd217524c2f05c00dd0396f60021e1
Denis Malinochkin <mrmlnc@yandex-team.ru>
2017-12-25 19:29:40 +0300

    chore(benchmark): Remove extra log from fixtures

--- src/benchmark/fixtures.ts
+++ src/benchmark/fixtures.ts
@@ -1,8 +1,6 @@
 import * as fs from 'fs-extra';
 import * as path from 'path';
 
-import * as logger from './logger';
-
 function writeFixtureFiles(basedir: string): void {
 	['txt', 'md'].forEach((extension) => fs.writeFileSync(`${basedir}/file.${extension}`, ''));
 }
@@ -11,8 +9,6 @@ export function makeFixtures(basedir: string, levelOfNesting: number): void {
 	let currentLevelDir = basedir;
 
 	fs.mkdirSync(currentLevelDir);
-
-	logger.subhead(`Creating ${levelOfNesting} level of fixtures`);
 
 	for (let level = 0; level < levelOfNesting; level++) {
 		currentLevelDir = path.join(currentLevelDir, level.toString());
@@ -23,6 +19,4 @@ export function makeFixtures(basedir: string, levelOfNesting: number): void {
 		writeFixtureFiles(currentLevelDir + '-a');
 		writeFixtureFiles(currentLevelDir + '-b');
 	}
-
-	logger.subhead(`Fixtures is created`);
 }
