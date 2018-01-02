a816c7bd059972cf7295701beea89a9324e5ef42
Denis Malinochkin <mrmlnc@yandex-team.ru>
2018-01-02 12:00:24 +0300

    feat(benchmark): Ability to run only sync or async suites

--- src/benchmark/index.ts
+++ src/benchmark/index.ts
@@ -12,6 +12,8 @@ const LEVEL: number = argv.level || 50;
 const RUNS: number = argv.runs || 100;
 const MAX_STDEV: number = argv.stdev || 5;
 const MAX_RETRIES: number = argv.retries || 5;
+const ASYNC_MODE: number = argv.sync || true;
+const SYNC_MODE: number = argv.sync || false;
 
 logger.head('Remove olded fixtures');
 rimraf.sync(BASE_BENCHMARK_PATH);
@@ -21,13 +23,17 @@ logger.head('Create fixtures');
 fixtures.makeFixtures(BASE_BENCHMARK_PATH, LEVEL);
 logger.newline();
 
-logger.head(`Benchmark for ${LEVEL} levels (runs: ${RUNS}) (async)`);
-runner.runSuites(BASE_BENCHMARK_PATH, 'async', RUNS, MAX_STDEV, MAX_RETRIES);
-logger.newline();
+if (ASYNC_MODE) {
+	logger.head(`Benchmark for ${LEVEL} levels (runs: ${RUNS}) (async)`);
+	runner.runSuites(BASE_BENCHMARK_PATH, 'async', RUNS, MAX_STDEV, MAX_RETRIES);
+	logger.newline();
+}
 
-logger.head(`Benchmark for ${LEVEL} levels (runs: ${RUNS}) (sync)`);
-runner.runSuites(BASE_BENCHMARK_PATH, 'sync', RUNS, MAX_STDEV, MAX_RETRIES);
-logger.newline();
+if (SYNC_MODE) {
+	logger.head(`Benchmark for ${LEVEL} levels (runs: ${RUNS}) (sync)`);
+	runner.runSuites(BASE_BENCHMARK_PATH, 'sync', RUNS, MAX_STDEV, MAX_RETRIES);
+	logger.newline();
+}
 
 logger.head('Remove fixtures');
 rimraf.sync(BASE_BENCHMARK_PATH);
