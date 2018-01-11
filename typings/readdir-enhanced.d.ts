53c3268fddb6fe7a79aec0be620d712f75dfb9a0
Denis Malinochkin <mrmlnc@yandex-team.ru>
2018-01-11 19:34:55 +0300

    chore(typings): Update readdir-enhanced types

--- typings/readdir-enhanced.d.ts
+++ typings/readdir-enhanced.d.ts
@@ -3,33 +3,48 @@ declare module "readdir-enhanced" {
 	import * as fs from 'fs';
 
 	namespace readdir {
+		type TFilterFunction = (stat: IEntry) => boolean;
+		type TReaddirFilterOption = string | RegExp | TFilterFunction;
+		type TReaddirDeepOption = boolean | number | RegExp | TFilterFunction;
+
+		type TCallback<T> = null | undefined | ((err: NodeJS.ErrnoException, result: T) => void)
+
 		interface IEntry extends fs.Stats {
 			path: string;
 			depth: number;
 		}
 
-		type TFilterFunction = (stat: IEntry) => boolean;
-		type TReaddirFilterOption = string | RegExp | TFilterFunction;
-		type TReaddirDeepOption = boolean | number | RegExp | TFilterFunction;
+		interface IFileSystem {
+			readdir?: (path: string, callback: TCallback<string[]>) => void;
+			lstat?: (path: string, callback: TCallback<fs.Stats>) => void;
+			stat?: (path: string, callback: TCallback<fs.Stats>) => void;
+		}
 
-		interface IReaddirOptions {
+		interface IReaddirOptions<T = any> {
 			filter?: TReaddirFilterOption;
 			deep?: TReaddirDeepOption;
 			sep?: string;
 			basePath?: string;
+			fs?: IFileSystem;
 		}
 
 		// Sync
-		function sync(root: string, options?: IReaddirOptions): string[];
 		function readdirSyncStat(root: string, options?: IReaddirOptions): IEntry[];
+		function sync(root: string, options?: IReaddirOptions): string[];
+		namespace sync {
+			function stat(root: string, options?: IReaddirOptions): string[];
+		}
 
 		// Promise
-		function async(root: string, options?: IReaddirOptions): Promise<string[]>;
 		function readdirAsyncStat(root: string, options?: IReaddirOptions): Promise<IEntry[]>;
+		function async(root: string, options?: IReaddirOptions): Promise<string[]>;
+		namespace async {
+			function stat(root: string, options?: IReaddirOptions): Promise<IEntry[]>;
+		}
 
 		// Callback
-		function async(root: string, cb: (err: Error, files: string[]) => void): void;
-		function async(root: string, options: IReaddirOptions, cb: (err: Error, files: string[]) => void): void;
+		function async(root: string, callback: TCallback<string[]>): void;
+		function async(root: string, options: IReaddirOptions, callback: TCallback<string[]>): void;
 
 		// Stream
 		function stream(root: string, options?: IReaddirOptions): NodeJS.ReadableStream;
