14434a720b1f87a09558917aba7e164141b082df
Denis Malinochkin <mrmlnc@yandex-team.ru>
2018-02-25 16:06:45 +0300

    feat(adapters): Add common FileSystem adapter

---
+++ src/adapters/fs.spec.ts
@@ -0,0 +1,70 @@
+import * as assert from 'assert';
+import * as fs from 'fs';
+import * as path from 'path';
+
+import FileSystem from './fs';
+
+import * as optionsManager from '../managers/options';
+
+import { Entry } from '../types/entries';
+
+class FileSystemFake extends FileSystem<never[]> {
+	public read(_filepaths: string[]): never[] {
+		return [];
+	}
+}
+
+function getAdapter(): FileSystemFake {
+	const options = optionsManager.prepare();
+
+	return new FileSystemFake(options);
+}
+
+describe('Adapters → FileSystem', () => {
+	describe('Constructor', () => {
+		it('should create instance of class', () => {
+			const adapter = getAdapter();
+
+			assert.ok(adapter instanceof FileSystem);
+		});
+	});
+
+	describe('.read', () => {
+		it('should return empty array', () => {
+			const adapter = getAdapter();
+
+			const expected: never[] = [];
+
+			const actual = adapter.read([]);
+
+			assert.deepEqual(actual, expected);
+		});
+	});
+
+	describe('.getFullEntryPath', () => {
+		it('should return path to entry', () => {
+			const adapter = getAdapter();
+
+			const expected = path.join(process.cwd(), 'config.json');
+
+			const actual = adapter.getFullEntryPath('config.json');
+
+			assert.equal(actual, expected);
+		});
+	});
+
+	describe('.makeEntry', () => {
+		it('should return created entry', () => {
+			const adapter = getAdapter();
+
+			const expected = {
+				path: 'base/file.json',
+				depth: 2
+			} as Entry;
+
+			const actual = adapter.makeEntry({} as fs.Stats, 'base/file.json');
+
+			assert.deepEqual(actual, expected);
+		});
+	});
+});
---
+++ src/adapters/fs.ts
@@ -0,0 +1,34 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+import { FilterFunction } from '@mrmlnc/readdir-enhanced';
+
+import { IOptions } from '../managers/options';
+import { Entry } from '../types/entries';
+import { Pattern } from '../types/patterns';
+
+export default abstract class FileSystem<T> {
+	constructor(private readonly options: IOptions) { }
+
+	/**
+	 * The main logic of reading the entries that must be implemented by each adapter.
+	 */
+	public abstract read(filepaths: string[], filter: FilterFunction): T;
+
+	/**
+	 * Return full path to entry.
+	 */
+	public getFullEntryPath(filepath: string): string {
+		return path.resolve(this.options.cwd, filepath);
+	}
+
+	/**
+	 * Return an implementation of the Entry interface.
+	 */
+	public makeEntry(stat: fs.Stats, pattern: Pattern): Entry {
+		return Object.assign<fs.Stats, Entry>(stat, {
+			path: pattern,
+			depth: pattern.split('/').length
+		} as Entry);
+	}
+}
