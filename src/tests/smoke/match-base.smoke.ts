c58ba78d86fbda58c1bd900d2ae9ba8ee6c2cfec
mrmlnc <mrmlnc@yandex-team.ru>
2019-06-10 10:24:34 +0300

    test(smoke): add some smoke tests

---
+++ src/tests/smoke/match-base.smoke.ts
@@ -0,0 +1,12 @@
+import * as smoke from './smoke';
+
+smoke.suite('Smoke → MatchBase', [
+	{
+		broken: true,
+		issue: 199,
+		pattern: '*.md',
+		cwd: 'fixtures',
+		globOptions: { matchBase: true },
+		fgOptions: { matchBase: true }
+	}
+]);
