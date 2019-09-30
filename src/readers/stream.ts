57425a0318aa9bd95a21afdec26281ba9ec9502e
mrmlnc <mrmlnc@yandex-team.ru>
2019-09-30 22:14:28 +0300

    style: fix `no-unnecessary-condition` rule

--- src/readers/stream.ts
+++ src/readers/stream.ts
@@ -57,7 +57,7 @@ export default class ReaderStream extends Reader<NodeJS.ReadableStream> {
 
 	private _getStat(filepath: string): Promise<fs.Stats> {
 		return new Promise((resolve, reject) => {
-			this._stat(filepath, this._fsStatSettings, (error, stats) => {
+			this._stat(filepath, this._fsStatSettings, (error: NodeJS.ErrnoException | null, stats) => {
 				error ? reject(error) : resolve(stats);
 			});
 		});
