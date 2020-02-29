cfe331ac3d9feb02803e715059524083aca8e674
mrmlnc <mrmlnc@yandex-team.ru>
2020-02-29 16:48:21 +0300

    refactor(filters/entries): improve unit tests

--- src/providers/filters/entry.spec.ts
+++ src/providers/filters/entry.spec.ts
@@ -1,9 +1,19 @@
 import * as assert from 'assert';
+import * as path from 'path';
 
 import Settings, { Options } from '../../settings';
 import * as tests from '../../tests';
-import { EntryFilterFunction, Pattern } from '../../types';
+import { EntryFilterFunction, Pattern, Entry } from '../../types';
 import EntryFilter from './entry';
+
+type FilterOptions = {
+	positive: Pattern[];
+	negative?: Pattern[];
+	options?: Options;
+};
+
+const FILE_ENTRY = tests.entry.builder().path('root/file.txt').file().build();
+const DIRECTORY_ENTRY = tests.entry.builder().path('root/directory').directory().build();
 
 function getEntryFilterInstance(options?: Options): EntryFilter {
 	const settings = new Settings(options);
@@ -13,8 +23,24 @@ function getEntryFilterInstance(options?: Options): EntryFilter {
 	});
 }
 
-function getFilter(positive: Pattern[], negative: Pattern[], options?: Options): EntryFilterFunction {
-	return getEntryFilterInstance(options).getFilter(positive, negative);
+function getFilter(options: FilterOptions): EntryFilterFunction {
+	const negative = options.negative ?? [];
+
+	return getEntryFilterInstance(options.options).getFilter(options.positive, negative);
+}
+
+function getResult(entry: Entry, options: FilterOptions): boolean {
+	const filter = getFilter(options);
+
+	return filter(entry);
+}
+
+function accept(entry: Entry, options: FilterOptions): void {
+	assert.strictEqual(getResult(entry, options), true);
+}
+
+function reject(entry: Entry, options: FilterOptions): void {
+	assert.strictEqual(getResult(entry, options), false);
 }
 
 describe('Providers → Filters → Entry', () => {
@@ -28,165 +54,181 @@ describe('Providers → Filters → Entry', () => {
 
 	describe('.getFilter', () => {
 		describe('options.unique', () => {
-			it('should return `false` when an option is enabled', () => {
-				const filter = getFilter(['**/*'], []);
+			it('should do not build the index when an option is disabled', () => {
+				const filterInstance = getEntryFilterInstance({ unique: false });
 
-				const entry = tests.entry.builder().path('root/file.txt').file().build();
+				const filter = filterInstance.getFilter(['**/*'], []);
+
+				filter(FILE_ENTRY);
+
+				assert.strictEqual(filterInstance.index.size, 0);
+			});
+
+			it('should reject a duplicate entry', () => {
+				const filter = getFilter({
+					positive: ['**/*']
+				});
 
 				// Create index record
-				filter(entry);
+				filter(FILE_ENTRY);
 
-				const actual = filter(entry);
+				const actual = filter(FILE_ENTRY);
 
 				assert.ok(!actual);
 			});
 
-			it('should not build the index when an option is disabled', () => {
-				const filterInstance = getEntryFilterInstance({ onlyFiles: false, unique: false });
+			it('should accept a duplicate entry', () => {
+				const filter = getFilter({
+					positive: ['**/*'],
+					options: { unique: false }
+				});
 
-				const filter = filterInstance.getFilter(['**/*'], []);
+				// Create index record
+				filter(FILE_ENTRY);
 
-				const entry = tests.entry.builder().path('root/file.txt').file().build();
+				const actual = filter(FILE_ENTRY);
 
-				filter(entry);
-
-				assert.strictEqual(filterInstance.index.size, 0);
+				assert.ok(actual);
 			});
 		});
 
 		describe('options.onlyFiles', () => {
-			it('should return `false` for the directory entry when an option is enabled', () => {
-				const filter = getFilter(['**/*'], []);
-
-				const entry = tests.entry.builder().path('root/directory').directory().build();
-
-				const actual = filter(entry);
-
-				assert.ok(!actual);
+			it('should reject a directory entry', () => {
+				reject(DIRECTORY_ENTRY, {
+					positive: ['**/*'],
+					options: { onlyFiles: true }
+				});
 			});
 
-			it('should return `true` for the file entry when an option is enabled', () => {
-				const filter = getFilter(['**/*'], []);
+			it('should accept a directory entry', () => {
+				accept(DIRECTORY_ENTRY, {
+					positive: ['**/*'],
+					options: { onlyFiles: false }
+				});
+			});
 
-				const entry = tests.entry.builder().path('root/file.txt').file().build();
-
-				const actual = filter(entry);
-
-				assert.ok(actual);
+			it('should accept a file entry', () => {
+				accept(FILE_ENTRY, {
+					positive: ['**/*'],
+					options: { onlyFiles: true }
+				});
 			});
 		});
 
 		describe('options.onlyDirectories', () => {
-			it('should return `false` for the directory entry when an option is enabled', () => {
-				const filter = getFilter(['**/*'], [], { onlyDirectories: true });
-
-				const entry = tests.entry.builder().path('root/file.txt').file().build();
-
-				const actual = filter(entry);
-
-				assert.ok(!actual);
+			it('should reject a file entry', () => {
+				reject(FILE_ENTRY, {
+					positive: ['**/*'],
+					options: { onlyDirectories: true }
+				});
 			});
 
-			it('should return `true` for the directory entry when an option is enabled', () => {
-				const filter = getFilter(['**/*'], [], { onlyDirectories: true });
-
-				const entry = tests.entry.builder().path('root/directory').directory().build();
-
-				const actual = filter(entry);
-
-				assert.ok(actual);
+			it('should accept a directory entry', () => {
+				accept(DIRECTORY_ENTRY, {
+					positive: ['**/*'],
+					options: { onlyDirectories: true }
+				});
 			});
 		});
 
 		describe('options.absolute', () => {
-			it('should return `false` when an entry match to the negative pattern', () => {
-				const filter = getFilter(['**/*'], ['**/*'], { absolute: true });
-
-				const entry = tests.entry.builder().path('root/file.txt').file().build();
-
-				const actual = filter(entry);
-
-				assert.ok(!actual);
+			it('should reject when an entry match to the negative pattern', () => {
+				reject(FILE_ENTRY, {
+					positive: ['**/*'],
+					negative: ['**/*'],
+					options: { absolute: true }
+				});
 			});
 
-			it('should return `true` when an entry do not match to the negative pattern', () => {
-				const filter = getFilter(['**/*'], ['*'], { absolute: true });
+			it('should reject when an entry match to the negative pattern with absolute path', () => {
+				const negative = path.posix.join(process.cwd().replace(/\\/g, '/'), '**', '*');
 
-				const entry = tests.entry.builder().path('root/file.txt').file().build();
+				reject(FILE_ENTRY, {
+					positive: ['**/*'],
+					negative: [negative],
+					options: { absolute: true }
+				});
+			});
 
-				const actual = filter(entry);
+			it('should accept when an entry does not match to the negative pattern', () => {
+				accept(FILE_ENTRY, {
+					positive: ['**/*'],
+					negative: ['*'],
+					options: { absolute: true }
+				});
+			});
 
-				assert.ok(actual);
+			it('should accept when an entry does not match to the negative pattern with absolute path', () => {
+				const negative = path.posix.join(process.cwd().replace(/\\/g, '/'), 'non-root', '**', '*');
+
+				accept(FILE_ENTRY, {
+					positive: ['**/*'],
+					negative: [negative],
+					options: { absolute: true }
+				});
 			});
 		});
 
 		describe('options.baseNameMatch', () => {
-			it('should return `false` when an option is disabled', () => {
-				const filter = getFilter(['*'], []);
-
-				const entry = tests.entry.builder().path('root/file.txt').file().build();
-
-				const actual = filter(entry);
-
-				assert.ok(!actual);
+			it('should reject an entry', () => {
+				reject(FILE_ENTRY, {
+					positive: ['*'],
+					options: { baseNameMatch: false }
+				});
 			});
 
-			it('should return `true` when an option is enabled', () => {
-				const filter = getFilter(['*'], [], { baseNameMatch: true });
-
-				const entry = tests.entry.builder().path('root/file.txt').file().build();
-
-				const actual = filter(entry);
-
-				assert.ok(actual);
+			it('should accept an entry', () => {
+				accept(FILE_ENTRY, {
+					positive: ['*'],
+					options: { baseNameMatch: true }
+				});
 			});
 		});
 
 		describe('Pattern', () => {
-			it('should return `false` when an entry match to the negative pattern', () => {
-				const filter = getFilter(['**/*'], ['**/*']);
-				const entry = tests.entry.builder().path('root/file.txt').file().build();
-
-				const actual = filter(entry);
-
-				assert.ok(!actual);
+			it('should reject when an entry match to the negative pattern', () => {
+				reject(FILE_ENTRY, {
+					positive: ['**/*'],
+					negative: ['**/*']
+				});
 			});
 
-			it('should return `false` when an entry do not match to the positive pattern', () => {
-				const filter = getFilter(['*'], []);
-				const entry = tests.entry.builder().path('root/file.txt').file().build();
-
-				const actual = filter(entry);
-
-				assert.ok(!actual);
+			it('should reject when an entry does not match to the positive pattern', () => {
+				reject(FILE_ENTRY, {
+					positive: ['*']
+				});
 			});
 
-			it('should return `true` when an entry match to the positive pattern with leading dot', () => {
-				const filter = getFilter(['./**/*'], []);
+			it('should accept when an entry match to the positive pattern with a leading dot', () => {
+				accept(FILE_ENTRY, {
+					positive: ['./**/*']
+				});
+			});
+
+			it('should accept an entry with a leading dot', () => {
 				const entry = tests.entry.builder().path('./root/file.txt').file().build();
 
-				const actual = filter(entry);
-
-				assert.ok(actual);
+				accept(entry, {
+					positive: ['**/*']
+				});
 			});
 
-			it('should return `true` when an entry match to the positive pattern', () => {
-				const filter = getFilter(['**/*'], []);
-				const entry = tests.entry.builder().path('root/file.txt').file().build();
-
-				const actual = filter(entry);
-
-				assert.ok(actual);
+			it('should accept when an entry match to the positive pattern', () => {
+				accept(FILE_ENTRY, {
+					positive: ['**/*']
+				});
 			});
 		});
 	});
 
 	describe('Immutability', () => {
 		it('should return the data without changes', () => {
-			const filter = getFilter(['**/*'], []);
+			const filter = getFilter({
+				positive: ['**/*']
+			});
 
-			const reference = tests.entry.builder().path('root/file.txt').directory().build();
-			const entry = tests.entry.builder().path('root/file.txt').directory().build();
+			const reference = tests.entry.builder().path('root/file.txt').file().build();
+			const entry = tests.entry.builder().path('root/file.txt').file().build();
 
 			filter(entry);
 
