e902e4453131d19e60778a75976e543ca899fb98
mrmlnc <dmalinochkin@outlook.com>
2023-07-23 18:40:03 +0300

    style: apply 'mocha/valid-test-description' rule

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -10,7 +10,7 @@ const invalidInputData = null as unknown as string;
 
 describe('Package', () => {
 	describe('.globSync', () => {
-		it('must be an alias for the .sync method', () => {
+		it('should be an alias for the .sync method', () => {
 			assert.strictEqual(fg.globSync, fg.sync);
 		});
 	});
@@ -62,7 +62,7 @@ describe('Package', () => {
 	});
 
 	describe('.glob', () => {
-		it('must be an alias for the .sync method', () => {
+		it('should be an alias for the .sync method', () => {
 			assert.strictEqual(fg.glob, fg.async);
 		});
 	});
@@ -114,7 +114,7 @@ describe('Package', () => {
 	});
 
 	describe('.globStream', () => {
-		it('must be an alias for the .sync method', () => {
+		it('should be an alias for the .sync method', () => {
 			assert.strictEqual(fg.globStream, fg.stream);
 		});
 	});
