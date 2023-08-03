49b8b9f77c13e9ddaafe2d597cf8cb3113c11dce
mrmlnc <dmalinochkin@outlook.com>
2023-08-03 12:30:08 +0300

    style: apply 'prefer-template' rule

--- src/utils/pattern.ts
+++ src/utils/pattern.ts
@@ -86,7 +86,7 @@ export function convertToPositivePattern(pattern: Pattern): Pattern {
 }
 
 export function convertToNegativePattern(pattern: Pattern): Pattern {
-	return '!' + pattern;
+	return `!${pattern}`;
 }
 
 export function isNegativePattern(pattern: Pattern): boolean {
@@ -140,7 +140,7 @@ export function hasGlobStar(pattern: Pattern): boolean {
 }
 
 export function endsWithSlashGlobStar(pattern: Pattern): boolean {
-	return pattern.endsWith('/' + GLOBSTAR);
+	return pattern.endsWith(`/${GLOBSTAR}`);
 }
 
 export function isAffectDepthOfReadingPattern(pattern: Pattern): boolean {
